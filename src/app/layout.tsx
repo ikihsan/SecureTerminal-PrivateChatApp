984a684d34769aa2ff101e76835b7037508bdc8b
ikihsan <ihsanpops@gmail.com>
2025-11-17 14:09:39 +0530

    speed Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { JetBrains_Mono } from 'next/font/google'
 import ConvexProviderWrapper from '../components/ConvexProviderWrapper'
 import './globals.css'
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 const jetbrainsMono = JetBrains_Mono({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -25,6 +26,7 @@ export default function RootLayout({
         <ConvexProviderWrapper>
           {children}
           <Analytics />
+          <SpeedInsights />
         </ConvexProviderWrapper>
       </body>
     </html>
